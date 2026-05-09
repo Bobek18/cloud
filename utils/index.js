45e6a0eb2c02356971e7677c83cd2d53d41d909a
thesuspect901 <152015392+Bobek18@users.noreply.github.com>
2026-05-09 20:26:52 +0300

    add load generator

---
+++ utils/index.js
@@ -0,0 +1,31 @@
+const url = "https://lab2cloudrun.delightfulwave-3836850b.polandcentral.azurecontainerapps.io/pizza";
+const TOKEN = ""; 
+
+const data = {
+  id: 0,
+  name: "string",
+  description: "string",
+  price: 0,
+  size: "string",
+  ingredients: ["string"]
+};
+
+async function sendRequest(i) {
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${TOKEN}`
+    },
+    body: JSON.stringify(data)
+  });
+  console.log(`Request ${i + 1}: ${res.status}`);
+}
+
+(async () => {
+  const tasks = [];
+  for (let i = 0; i < 1000; i++) {
+    tasks.push(sendRequest(i));
+  }
+  await Promise.all(tasks);
+})();
